1e1d3d80353b9f516e6946bb3242f3195d9c02e4
emoie <64896179+emoie@users.noreply.github.com>
2021-01-15 17:18:14 +0530

    Update DbConfiguration.java

--- src/main/java/com/supermarket/sevenstar/dbconfig/DbConfiguration.java
+++ src/main/java/com/supermarket/sevenstar/dbconfig/DbConfiguration.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbConfiguration {
 
 	private final static String driverClassName ="com.mysql.cj.jdbc.Driver";
-	private final static String databaseURL = "jdbc:mysql://us-cdbr-east-02.cleardb.com:3306/heroku_24b38c9f271006333";
+	private final static String databaseURL = "jdbc:mysql://us-cdbr-east-02.cleardb.com:3306/heroku_24b38c9f27100633";
 	private final static String databaseUsername = "bde75ebd4a2611";
 	private final static String databasePassword = "72095536";
 	
